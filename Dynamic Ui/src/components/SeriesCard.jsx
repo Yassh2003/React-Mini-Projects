8a6def03f732512e6298e3aad8f7f8eaf3750ca1
Yassh2003 <yashpratapindia789@gmail.com>
2024-12-13 19:31:15 +0530

    fixed array de-structure

--- Dynamic Ui/src/components/SeriesCard.jsx
+++ Dynamic Ui/src/components/SeriesCard.jsx
@@ -1,18 +1,18 @@
 export const SeriesCard = (props) => {
 
-    const { data } = props;
+    const { img_url, name, rating, description, genre, cast, watch_url } = data.props;
 
     return ( 
         <li>
             <div>
-                <img src={data.img_url} alt={data.name} width="40%" height="40%"/>
+                <img src={img_url} alt={name} width="40%" height="40%"/>
             </div>
-            <h2>Name: {data.name}</h2>
-            <h3>Rating: {data.rating} </h3>
-            <p>Summary : {data.description} </p>
-            <p>Genre: {data.genre} </p>
-            <p>Cast: {data.cast}</p>
-            <a href={data.watch_url} target="_blank">
+            <h2>Name: {name}</h2>
+            <h3>Rating: {rating} </h3>
+            <p>Summary : {description} </p>
+            <p>Genre: {genre} </p>
+            <p>Cast: {cast}</p>
+            <a href={watch_url} target="_blank">
                 <button>Watch Now</button>
             </a>
         </li>
