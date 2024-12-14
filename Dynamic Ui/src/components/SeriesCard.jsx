3d9b74a58638a21f80886c6dbfcf28d080a5b61d
Yassh2003 <yashpratapindia789@gmail.com>
2024-12-14 07:44:16 +0530

    added inline css

--- Dynamic Ui/src/components/SeriesCard.jsx
+++ Dynamic Ui/src/components/SeriesCard.jsx
@@ -2,19 +2,30 @@ export const SeriesCard = (props) => {
 
     const { img_url, name, rating, description, genre, cast, watch_url } = props.data;
 
+    const btn_style = {
+        padding: "1.2rem 2.4rem",
+        border:"none",
+        fontSize:"1.6rem",
+        backgroundColor: "var( --bnt-hover-bg-color)",
+        color: "var( --bg-color)",
+        cursor: "pointer",
+    }
+
     return ( 
-        <li>
+        <li className="card">
             <div>
                 <img src={img_url} alt={name} width="40%" height="40%"/>
             </div>
-            <h2>Name: {name}</h2>
-            <h3>Rating: {rating} </h3>
-            <p>Summary : {description} </p>
-            <p>Genre: {genre} </p>
-            <p>Cast: {cast}</p>
-            <a href={watch_url} target="_blank">
-                <button>Watch Now</button>
-            </a>
+            <div className="card-content">
+                <h2>Name: {name}</h2>
+                <h3>Rating: {rating} </h3>
+                <p>Summary : {description} </p>
+                <p>Genre: {genre} </p>
+                <p>Cast: {cast}</p>
+                <a href={watch_url} target="_blank">
+                    <button style={btn_style}>Watch Now</button>
+                </a>
+            </div>
         </li>
     );
 }
